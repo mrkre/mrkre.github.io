d6a3d1b2806d98db470b4340da35f52a002e985d
yaohong <yaohong@stashaway.com>
2020-08-07 15:34:32 +0800

    Build Fri Aug  7 15:34:32 +08 2020

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# generate
+hugo
+
+# add commit
+git add -A
+
+msg="Build `date`"
+
+if [ $# -eq 1]
+  then msg="$1"
+fi
+
+# push to master
+git commit -m "$msg"
+
+git push origin master
+
+cd ..
